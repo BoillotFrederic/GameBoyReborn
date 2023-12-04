32e64af1718934ee3fff4a53e8405cef5546936c
BoillotFrederic <boillot.frederic.62@gmail.com>
2023-12-04 19:21:24 +0100

    Add base CPU

---
+++ GameBoyReborn/Cores/CPU/CPU.cs
@@ -0,0 +1,30 @@
+﻿// ---
+// CPU
+// ---
+public class CPU
+{
+    // Construct
+    private Instructions Instructions;
+    public State State;
+
+    public CPU()
+    {
+        Instructions = new Instructions(this);
+        State = new State(this);
+    }
+
+    // Cycles
+    public int Cycles;
+
+    // Program counter
+    public ushort PC;
+
+    // Interrupt
+    public bool IME;
+    public bool IME_scheduled;
+
+    // Execution
+    public void Execution()
+    {
+    }
+}
