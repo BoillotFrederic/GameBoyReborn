dbde99937f2dfe86c737ca8cb51a9a8c3a0b3717
BoillotFrederic <boillot.frederic.62@gmail.com>
2023-12-10 20:16:34 +0100

    Add async task for debug log

--- GameBoyReborn/Program.cs
+++ GameBoyReborn/Program.cs
@@ -10,12 +10,20 @@ public class Program
 
     public static Task Main(string[] args)
     {
+        // Debug log
+        WriteLog().GetAwaiter().GetResult();
+
         // Set Raylib
+        Raylib.SetTraceLogLevel(TraceLogLevel.LOG_WARNING);
         Raylib.InitWindow(800, 600, "GameBoyReborn");
         Raylib.SetTargetFPS(60);
 
         // Init screen image
         Drawing.ScreenImage = Raylib.GenImageColor(160, 144, Color.RAYWHITE);
+
+        // Load game
+        Emulation.Load("Tetris.gb");
+        Emulation.Init();
 
         // Game loop
         while (!Raylib.WindowShouldClose())
@@ -30,5 +38,20 @@ public class Program
         // Exit program
         Raylib.CloseWindow();
         return Task.CompletedTask;
+    }
+
+    // Write debug log
+    private static async Task WriteLog()
+    {
+        string logFilePath = "log.txt";
+
+        using (StreamWriter streamWriter = new StreamWriter(logFilePath))
+        {
+            Console.SetOut(streamWriter);
+            Console.WriteLine("Starting the program...");
+
+            await Task.Delay(1000);
+            streamWriter.Close();
+        }
     }
 }
