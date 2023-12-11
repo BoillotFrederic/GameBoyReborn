8bf3d9c945ce57c74b80525358b31a904df537b9
BoillotFrederic <boillot.frederic.62@gmail.com>
2023-12-11 20:18:51 +0100

    Moving file

---
+++ GameBoyReborn/Cores/State.cs
@@ -0,0 +1,25 @@
+﻿// -----
+// State
+// -----
+public class State
+{
+    // Construct
+    private CPU CPU;
+
+    public State(CPU cpu)
+    {
+        this.CPU = cpu;
+    }
+
+    public void Save()
+    {
+    }
+
+    public void Load()
+    {
+    }
+
+    public void Rewind()
+    {
+    }
+}
