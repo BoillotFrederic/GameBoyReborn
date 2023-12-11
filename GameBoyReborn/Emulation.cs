599364d823be3a827d30081a5b61db2b07d2c0e8
BoillotFrederic <boillot.frederic.62@gmail.com>
2023-12-11 20:21:48 +0100

    Emulation which follows is in progress

--- GameBoyReborn/Emulation.cs
+++ GameBoyReborn/Emulation.cs
@@ -6,7 +6,10 @@ using System.IO;
 
 public static class Emulation
 {
-    private static byte[] romData;
+    private static byte[]? romData;
+    private static Cartridge? Cartridge;
+    private static Memory? Memory;
+    private static CPU? CPU;
 
     // All cores init
     public static void Init()
@@ -16,15 +19,16 @@ public static class Emulation
             byte[] header = new byte[0x1C];
             Array.Copy(romData, 0x0134, header, 0, 0x1C);
 
-            CPU CPU = new CPU();
-            Cartridge Cartridge = new Cartridge(header);
+            Cartridge = new Cartridge(header);
+            Memory = new Memory(Cartridge, romData);
+            CPU = new CPU(Memory);
 
             Debug.Text(Cartridge.Title, Color.BLACK, 10000);
             Debug.Text(Cartridge.ManufacturerCode, Color.BLACK, 10000);
             Debug.Text(Cartridge.CGBDescription, Color.BLACK, 10000);
             Debug.Text(Cartridge.Licensee, Color.BLACK, 10000);
             Debug.Text(Cartridge.SGBDescription, Color.BLACK, 10000);
-            Debug.Text(Cartridge.Type, Color.BLACK, 10000);
+            Debug.Text(Cartridge.TypeDescription, Color.BLACK, 10000);
             Debug.Text(Cartridge.SizeDescription, Color.BLACK, 10000);
         }
     }
@@ -40,5 +44,13 @@ public static class Emulation
     public static void Loop()
     {
 
+    }
+
+    // Stop emulation
+    public static void Stop()
+    {
+        Cartridge = null;
+        Memory = null;
+        CPU = null;
     }
 }
