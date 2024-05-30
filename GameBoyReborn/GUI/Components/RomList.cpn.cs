a0571d0ae0d9a4783dd6e57441f2ee51fa61cae8
BoillotFrederic <boillot.frederic.62@gmail.com>
2024-05-30 20:17:45 +0200

    GUI : Separation and dynamization of the scrollbar component

--- GameBoyReborn/GUI/Components/RomList.cpn.cs
+++ GameBoyReborn/GUI/Components/RomList.cpn.cs
@@ -10,10 +10,8 @@ namespace GameBoyReborn
     {
         private static bool InputActionMove = false;
         private static bool ThumbnailClicked = false;
-        private static bool scrollClicked = false;
-        private static int scrollClickedPos = 0;
-        private static int scrollClickedLastPosList = 0;
-        private static int GameListTopShift = 100;
+        private static int PosListTop = 0;
+        private static bool ScrollVisible = false;
         private static int MouseLeftClickLastTarget = 0;
         private static int MouseLeftClickTarget = 0;
 
@@ -27,32 +25,26 @@ namespace GameBoyReborn
         // Draw list
         private static void DrawListGame()
         {
+
             // Operating variables
             ThumbnailClicked = false;
             bool focus = WhereIAm == "List";
-            float mouseWheelMove = focus ? Raylib.GetMouseWheelMove() : 0;
             int cartridgeHeight = Res(300);
             int yStart = Res(100);
             int yMargin = Res(65);
             double nbLine = Math.Ceiling(NbGame / 6.0f);
             int FullThumbnailHeight = cartridgeHeight + yMargin;
-            int scrollBarWidth = Res(30);
-            int scrollBarHeight = (int)((float)ScreenHeight / ((nbLine * FullThumbnailHeight) + (yStart * 2)) * ScreenHeight);
-            int cartridgeWidth = Res((scrollBarHeight > ScreenHeight) ? 400 : 395);
+            int cartridgeWidth = Res(!ScrollVisible ? 400 : 395);
             int gameListPageHeight = (int)(nbLine * FullThumbnailHeight);
-            int gameListTop = Res(GameListTopShift);
-            int gameListTopPrepare = GameListTopShift + (int)(mouseWheelMove * 100);
-            int gameListBottomLimit = gameListPageHeight - (ScreenHeight - yStart);
-            int scrollPos = (int)((float)gameListTop / gameListPageHeight * ScreenHeight) * -1;
             ThumbnailClicked = false;
 
             // Delemiter area
             InitScreenSize();
-            Raylib.BeginScissorMode(0, yStart - 1, ScreenWidth - scrollBarWidth, ScreenHeight - yStart * 2);
+            Raylib.BeginScissorMode(0, yStart - 1, ScreenWidth - Res(30), ScreenHeight - yStart * 2);
 
             // Draw line selected
             int selectedCartridgeX = MouseLeftClickTarget % 6 * cartridgeWidth;
-            int selectedCartridgeY = (int)(gameListTop + (Math.Floor(MouseLeftClickTarget / 6.0f) * FullThumbnailHeight));
+            int selectedCartridgeY = (int)(PosListTop + (Math.Floor(MouseLeftClickTarget / 6.0f) * FullThumbnailHeight));
             int selectedCartridgeHeight = FullThumbnailHeight + Res(5);
             Raylib.DrawRectangle(selectedCartridgeX, selectedCartridgeY, cartridgeWidth, selectedCartridgeHeight, Color.LIGHTGRAY);
             Raylib.DrawRectangleLines(selectedCartridgeX, selectedCartridgeY, cartridgeWidth, selectedCartridgeHeight, Color.GRAY);
@@ -73,7 +65,7 @@ namespace GameBoyReborn
 
                     // Where
                     int X = x * cartridgeWidth;
-                    int Y = gameListTop + (y * FullThumbnailHeight);
+                    int Y = PosListTop + (y * FullThumbnailHeight);
 
                     // Draw cartridge
                     Raylib.DrawTexture(CartridgeGB, X, Y, Color.WHITE);
@@ -135,74 +127,40 @@ namespace GameBoyReborn
             // Stop delimiter
             Raylib.EndScissorMode();
 
-            // Draw scrollbar
-            if (scrollBarHeight < ScreenHeight)
-            Raylib.DrawRectangle(ScreenWidth - scrollBarWidth, scrollPos, scrollBarWidth, scrollBarHeight, Color.DARKGRAY);
+            // Set scrollbar
+            string ScrollBarName = "ScrollList";
+            ScrollBarInit(ScrollBarName, 0);
+            PosListTop = Res(100) - (int)ScrollBarList[ScrollBarName].ContentPosY;
 
-            // Scrolling game list by click
-            Rectangle scrollRect = new()
-            {
-                X = ScreenWidth - scrollBarWidth,
-                Y = scrollPos,
-                Width = scrollBarWidth,
-                Height = scrollBarHeight
-            };
-
-            if (focus && Raylib.CheckCollisionPointRec(Mouse, scrollRect) && Input.Pressed("Click", Input.MouseLeftClick))
-            {
-                scrollClicked = true;
-                scrollClickedPos = (int)Mouse.Y;
-                scrollClickedLastPosList = GameListTopShift;
-            }
-
-            if (!Input.MouseLeftClick)
-            scrollClicked = false;
-
-            if (scrollClicked)
-            {
-                int shift = (int)(-1 * (float)((Mouse.Y - scrollClickedPos) / ScreenHeight) * gameListPageHeight);
-                gameListTopPrepare = (int)ResI((float)Res(scrollClickedLastPosList) + shift);
-            }
-
-            // Scrolling game list by mouse wheel
-            if (scrollBarHeight < ScreenHeight)
-            {
-                if (gameListTopPrepare >= 100)
-                GameListTopShift = 100;
-
-                else if(gameListTopPrepare < 0 && Res(gameListTopPrepare) * -1 > gameListBottomLimit)
-                GameListTopShift = (int)ResI(gameListBottomLimit * -1);
-
-                else if(gameListTopPrepare < 100)
-                GameListTopShift = gameListTopPrepare;
-            }
+            if (gameListPageHeight != 0)
+            ScrollVisible = ScrollBarY(ScrollBarName, ScreenHeight - Res(200), gameListPageHeight, Res(30), ScreenHeight, ScreenWidth - Res(30), 0, focus, Color.RAYWHITE, Color.DARKGRAY) != 1;
 
             // Hidden lines selected
-            selectedCartridgeY = (int)(gameListTop + (Math.Floor(MouseLeftClickTarget / 6.0f) * FullThumbnailHeight));
+            selectedCartridgeY = (int)(PosListTop + (Math.Floor(MouseLeftClickTarget / 6.0f) * FullThumbnailHeight));
             float nbLineDisplayed = (ScreenHeight - yStart * 2.0f) / FullThumbnailHeight;
             int heightDisplayed = (int)(nbLineDisplayed * FullThumbnailHeight);
             int lineRequested = (MouseLeftClickTarget / 6) + 1;
-            int lineTopHiddenRequestedY = (int)ResI(yStart + lineRequested * FullThumbnailHeight * -1 + FullThumbnailHeight);
-            int lineBottomHiddenRequestedY = (int)ResI(yStart + lineRequested * FullThumbnailHeight * -1 + heightDisplayed);
+            int lineTopHiddenRequestedY = lineRequested * FullThumbnailHeight - FullThumbnailHeight;
+            int lineBottomHiddenRequestedY = lineRequested * FullThumbnailHeight - heightDisplayed;
             bool topHidden = selectedCartridgeY < yStart;
             bool bottomHidden = selectedCartridgeY + FullThumbnailHeight > ScreenHeight - yStart + 1;
             bool topFullHidden = selectedCartridgeY + FullThumbnailHeight < yStart;
             bool bottomFullHidden = selectedCartridgeY > ScreenHeight - yStart;
 
-            if (topHidden && (InputActionMove || (ThumbnailClicked && !topFullHidden)) && !scrollClicked)
+            if (topHidden && (InputActionMove || (ThumbnailClicked && !topFullHidden)) && !ScrollBarList[ScrollBarName].HangClicked)
             {
-                GameListTopShift = lineTopHiddenRequestedY;
+                ScrollBarSetY(ScrollBarName, lineTopHiddenRequestedY);
                 ThumbnailClicked = false;
             }
-            if (bottomHidden && (InputActionMove || (ThumbnailClicked && !bottomFullHidden)) && !scrollClicked)
+            if (bottomHidden && (InputActionMove || (ThumbnailClicked && !bottomFullHidden)) && !ScrollBarList[ScrollBarName].HangClicked)
             {
-                GameListTopShift = lineBottomHiddenRequestedY;
+                ScrollBarSetY(ScrollBarName, lineBottomHiddenRequestedY);
                 ThumbnailClicked = false;
             }
 
             InputActionMove = false;
 
-            // Launch game
+            // Launch game by double click
             if (Input.MouseLeftDoubleClick && ThumbnailClicked && MouseLeftClickLastTarget == MouseLeftClickTarget)
             Action("ListPlay");
         }
--- GameBoyReborn/GUI/Components/Scroll.cpn.cs
+++ GameBoyReborn/GUI/Components/Scroll.cpn.cs
@@ -1,11 +1,133 @@
-﻿// -------
-// Actions
-// -------
+﻿// ---------
+// Scrollbar
+// ---------
+
+using Raylib_cs;
 
 namespace GameBoyReborn
 {
     public partial class DrawGUI
     {
+        // Operating variables
+        private static readonly Dictionary<string, ScrollBar> ScrollBarList = new();
 
+        // Structures
+        private struct ScrollBar
+        {
+            public bool HangClicked = false;
+            public float HangClickedPosY = 0;
+            public float ContentPosY = 0;
+            public float MousePos = 0;
+        }
+
+        // Init scrollbar
+        private static void ScrollBarInit(string name, int contentPosOrigin)
+        {
+            if (!ScrollBarList.ContainsKey(name))
+            {
+                ScrollBar scrollBar = new();
+                scrollBar.ContentPosY = contentPosOrigin;
+                ScrollBarList.Add(name, scrollBar);
+            }
+        }
+
+        // Set content Y
+        private static void ScrollBarSetY(string name, int newPosY)
+        {
+            ScrollBar scrollBar = ScrollBarList[name];
+            scrollBar.ContentPosY = newPosY;
+            ScrollBarList[name] = scrollBar;
+        }
+
+        // Set scrollbar
+        private static float ScrollBarY(string name, int containerHeight, int contentHeight, int scrollBarWidth, int scrollBarHeight, int scrollBarX, int scrollBarY, bool focused, Color colorBar, Color colorHang)
+        {
+            // First, draw
+            // -----------
+
+            // Get parameters
+            ScrollBar scrollBar = ScrollBarList[name];
+
+            // Scroll height in float percent
+            float scrollHeightPercent = contentHeight > containerHeight ? ((float)containerHeight / contentHeight) : 1;
+
+            // If no scroll, return 100%
+            if (scrollHeightPercent == 1)
+            return 1;
+
+            // Calculate position and size
+            int scrollBarHangHeight = (int)(scrollHeightPercent * scrollBarHeight);
+            int contentNotVisibleHeight = contentHeight - containerHeight;
+            int scrollBarEmptyHeight = scrollBarHeight - scrollBarHangHeight;
+            float ContentPosYPercent = scrollBar.ContentPosY / contentNotVisibleHeight;
+
+            // Limit hang
+            ContentPosYPercent = Math.Clamp(ContentPosYPercent, 0, 1);
+
+            // Set pos
+            int scrollBarPosY = (int)(ContentPosYPercent * scrollBarEmptyHeight);
+
+            // Scroll bar dimensions
+            Rectangle scrollBarRect = new()
+            {
+                X = scrollBarX,
+                Y = scrollBarY,
+                Width = scrollBarWidth,
+                Height = scrollBarHeight,
+            };
+
+            // Scroll bar hang dimensions
+            Rectangle scrollBarHangRectHang = new()
+            {
+                X = scrollBarX,
+                Y = scrollBarY + scrollBarPosY,
+                Width = scrollBarWidth,
+                Height = scrollBarHangHeight,
+            };
+
+            // Draw scroll bar / hang
+            Raylib.DrawRectangleRec(scrollBarRect, colorBar);
+            Raylib.DrawRectangleRec(scrollBarHangRectHang, colorHang);
+
+            // Second, event management
+            // ------------------------
+
+            // New position after event
+            int newContentPosY = 0;
+
+            // If not focused, stop event
+            if (!focused)
+            return scrollHeightPercent;
+
+            // Click pressed
+            if (Raylib.CheckCollisionPointRec(Mouse, scrollBarHangRectHang) && Input.Pressed("Click", Input.MouseLeftClick))
+            {
+                scrollBar.HangClicked = true;
+                scrollBar.HangClickedPosY = scrollBarPosY;
+                scrollBar.MousePos = Mouse.Y;
+            }
+
+            // Click unpressed
+            if (!Input.MouseLeftClick)
+            scrollBar.HangClicked = false;
+
+            // Move by wheel
+            if (!scrollBar.HangClicked)
+            newContentPosY = (int)(scrollBar.ContentPosY + (int)(Raylib.GetMouseWheelMove() * -100));
+
+            // Move with mouse
+            else if (scrollBar.HangClicked)
+            {
+                int hangPosDiff = (int)(scrollBar.HangClickedPosY  + (Mouse.Y - scrollBar.MousePos));
+                float diffToPercent = (float)hangPosDiff / scrollBarEmptyHeight;
+                newContentPosY = (int)(contentNotVisibleHeight * diffToPercent);
+            }
+
+            // Save parameters
+            scrollBar.ContentPosY = Math.Clamp(newContentPosY, 0, contentNotVisibleHeight);
+            ScrollBarList[name] = scrollBar;
+
+            return scrollHeightPercent;
+        }
     }
 }
